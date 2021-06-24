180fccc090cb4c9dd36f9ca2c85640474f82ed71
AlbertChien <albert.chien@moneybook.com.tw>
2021-06-24 20:45:47 +0800

    [feat] modify AppDelegate,SceneDelegate

--- SnapaskHomework1/Manager/App/AppManager.swift
+++ SnapaskHomework1/Manager/App/AppManager.swift
@@ -21,17 +21,17 @@ class AppManager {
         self.keyChain = keyChain
     }
     
-//    func start(in window: UIWindow?) {
-//        let vc = getCurrentRootViewController()
-//        let navi = UINavigationController(rootViewController: vc)
-//        window?.rootViewController = navi
-//        window?.makeKeyAndVisible()
-//    }
-//
-//    func getCurrentRootViewController()->UIViewController {
-//        let loginAuthViewController = LoginAuthViewController()
-//        let userListViewController = UserListViewController()
-//        let navi = isAuthed ? userListViewController : loginAuthViewController
-//        return navi
-//    }
+    func start(in window: UIWindow?) {
+        let vc = getCurrentRootViewController()
+        let navi = UINavigationController(rootViewController: vc)
+        window?.rootViewController = navi
+        window?.makeKeyAndVisible()
+    }
+
+    func getCurrentRootViewController()->UIViewController {
+        let loginAuthViewController = LoginAuthViewController()
+        let userListViewController = UserListViewController()
+        let navi = isAuthed ? userListViewController : loginAuthViewController
+        return navi
+    }
 }
--- SnapaskHomework1/SceneDelegate.swift
+++ SnapaskHomework1/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import OAuthSwift
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -13,10 +14,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        (UIApplication.shared.delegate as? AppDelegate)?.self.window = window
+        self.window = window
+        AppManager.shared.start(in: window)
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -47,6 +49,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // to restore the scene back to its current state.
     }
 
-
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        guard let url = URLContexts.first?.url else {
+            return
+        }
+        if url.host == "oauth-callback" {
+            OAuthSwift.handle(url: url)
+        }
+    }
 }
 
